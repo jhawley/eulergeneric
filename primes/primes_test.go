e7995dceec2db43ec6cda2510f1a65280480fe0d
Jeff Hawley <jhawley@vt.edu>
2014-04-23 11:42:21 -0400

    created a concurrent version of the sieve

--- primes/primes_test.go
+++ primes/primes_test.go
@@ -7,4 +7,33 @@ func TestSieveOfEratosthenes(t *testing.T) {
     if len(primes) == 4 && primes[3] != 7 {
         t.Error("SieveOfEratosthenes failed to return primes correctly")
     }
+    primes = SieveOfEratosthenes(11)
+    if len(primes) == 5 && primes[4] != 11 {
+        t.Error("SieveOfEratosthenes failed to return primes correctly")
+    }
+    primes = SieveOfEratosthenes(10000)
+}
+
+func TestSieveOfEratosthenesSequential(t *testing.T) {
+    primes := SieveOfEratosthenes(10)
+    if len(primes) == 4 && primes[3] != 7 {
+        t.Error("SieveOfEratosthenesSequential failed to return primes correctly")
+    }
+    primes = SieveOfEratosthenes(11)
+    if len(primes) == 5 && primes[4] != 11 {
+        t.Error("SieveOfEratosthenesSequential failed to return primes correctly")
+    }
+    primes = SieveOfEratosthenes(10000)
+}
+
+func BenchmarkSieveOfEratosthenes100000(b *testing.B) {
+    for i := 0 ; i < b.N; i++ {
+        SieveOfEratosthenes(100000)
+    }
+}
+
+func BenchmarkSieveOfEratosthenesSequential100000(b *testing.B) {
+    for i := 0 ; i < b.N; i++ {
+        SieveOfEratosthenesSequential(100000)
+    }
 }
